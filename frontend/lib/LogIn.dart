6c4292ea647a61db82cf3686dfb1d5b11eeabde9
Joanna Chauvin <haku65@o2.pl>
2024-11-06 21:59:27 +0100

    create classes  for LogIn Screen

---
+++ frontend/lib/LogIn.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatefulWidget{
+  const LogInScreen({super.key});
+  @override
+  State<LogInScreen> createState() {
+    return _LogInScreenState();
+  }
+
+
+}
+
+class _LogInScreenState extends State<LogInScreen>{
+  @override
+  Widget build(context) {
+    return const Text('LogIn Screen');
+  }
+
+}
