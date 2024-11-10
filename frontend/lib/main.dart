df2dcde8a869123178e58d8c63d9143401a0a305
rolbrown <49118629+rolbrown@users.noreply.github.com>
2024-11-10 23:35:35 +0100

    Update main.dart
    web

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
+ if(kIsWeb){Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyAocdxKkpHf2hDQ4QlZtT_2HKU-ThEspUI",
+  authDomain: "projekt-24-a9104.firebaseapp.com",
+  projectId: "projekt-24-a9104",
+  storageBucket: "projekt-24-a9104.firebasestorage.app",
+  messagingSenderId: "351644604797",
+  appId: "1:351644604797:web:2279767fbcdab70688d414"));
+} else{
+   Firebase.initializeApp();
+}
+  
   runApp(
     MaterialApp(
       home: Scaffold(
