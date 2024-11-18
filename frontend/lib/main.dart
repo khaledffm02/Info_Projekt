4d91d6b0f18e03602300a9c9408f2cc9a0c177cc
Admin <khaled.ffm02@gmail.com>
2024-11-18 06:40:02 +0100

    added files and dependencies for firebase

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/FairShare.dart';
 import 'package:frontend/StartScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb) {
+    await Firebase.initializeApp(options: FirebaseOptions(
+        apiKey: "AIzaSyAocdxKkpHf2hDQ4QlZtT_2HKU-ThEspUI",
+        authDomain: "projekt-24-a9104.firebaseapp.com",
+        projectId: "projekt-24-a9104",
+        storageBucket: "projekt-24-a9104.firebasestorage.app",
+        messagingSenderId: "351644604797",
+        appId: "1:351644604797:web:2279767fbcdab70688d414"));
+  }
+  else{
+    await Firebase.initializeApp();
+  }
   runApp(FairShare());
 }
