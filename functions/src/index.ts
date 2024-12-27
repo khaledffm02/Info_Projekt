0f6a8364a21b88d6768e618d56ec342eec1de1fc
Roldon Brown <brownroldon@gmail.com>
2024-12-27 16:23:23 +0100

    Add user deletion sanity check

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -133,6 +133,14 @@ export const userDelete = onRequest(
   {cors: true},
   async (request, response) => {
     const {userID} = await getUserID(request);
+    const groups = await groupManager.getGroupsForUser(userID);
+    const hasZeroBalance = groups.every(
+      (group) => group.getBalanceForUser(userID) === 0
+    );
+    if (!hasZeroBalance) {
+      response.send({success: false});
+      return;
+    }
     await groupManager.deleteUser(userID);
     response.send({success: userID});
   }
