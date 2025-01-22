10a7b83eb3c2d2dc71a0fb6e46a6d738910e57c2
Roldon Brown <brownroldon@gmail.com>
2025-01-22 22:46:51 +0100

    send_invitations_endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -534,6 +534,46 @@ export const resetLoginAttempts = onRequest(
   }
 );
 
+export const sendInvitation = onRequest(
+  {cors: true, secrets: [emailAccount, emailPassword]},
+  async (request, response) => {
+    const groupID = request.query.groupID as string;
+    const email = request.query.email as string;
+    const {userID} = await getUserID(request);
+    if (!groupID || !userID || !email) {
+      response.send({
+        success: false,
+        message: "Missing parameters",
+        detailedMessage: {groupID, userID, email},
+      });
+      return;
+    }
+    const group = await groupManager.getGroup(groupID);
+    if (!group) {
+      response.send({
+        success: false,
+        message: "Group not found",
+        detailedMessage: {groupID},
+      });
+      return;
+    }
+    await sendMail({
+      account: emailAccount.value(),
+      password: emailPassword.value(),
+      from: "splidapp@project.com",
+      text: `Please enter the invitation code ${group.data.groupCode}`,
+      to: email,
+      subject: "Invitation to join a group",
+      html: `<b>Hello, 
+            you have been invited to join the group "${group.data.name}".
+            Please enter the invitation code ${group.data.groupCode} 
+            in the app to join.</b>`,
+    });
+
+    response.send({success: true});
+  }
+);
+
 exports.scheduledFunctionCrontab = onSchedule("*/5 * * * *", async () => {
   const groups = await groupManager.getGroupRemindersForDate();
   const getEndpoint = (groupID: string) =>
