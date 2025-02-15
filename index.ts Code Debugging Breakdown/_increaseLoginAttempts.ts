66906dd04312d2090a6bffdd9f10684c5616707d
Roldon Brown <brownroldon@gmail.com>
2025-02-15 12:47:19 +0100

    _increaseLoginAttempts.ts bdn

---
+++ index.ts Code Debugging Breakdown/_increaseLoginAttempts.ts
@@ -0,0 +1,17 @@
+import { onRequest } from "firebase-functions/v2/https";
+import { getUserID } from "./auth";
+import { groupManager } from "./config";
+
+export const increaseLoginAttempts = onRequest(
+    {cors: true},
+    async (request, response) => {
+      const email = request.query.email as string;
+      try {
+        const success = await groupManager.increaseLoginAttempts(email);
+        response.send({success});
+      } catch {
+        response.send({message: "User not found"}).status(404);
+        return;
+      }
+    }
+  );
