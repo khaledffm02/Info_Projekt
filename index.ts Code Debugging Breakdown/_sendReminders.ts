e4bee51ffd2af6b732dd31c86ffc1d6ff8b6f4f3
Roldon Brown <brownroldon@gmail.com>
2025-02-15 12:42:11 +0100

    _sendReminders.ts bdn

---
+++ index.ts Code Debugging Breakdown/_sendReminders.ts
@@ -0,0 +1,46 @@
+import { onRequest } from "firebase-functions/v2/https";
+import { getUserID } from "./auth";
+import { groupManager } from "./config";
+
+export const sendReminders = onRequest(
+    {cors: true, secrets: [emailAccount, emailPassword]},
+    async (request, response) => {
+      const groupID = request.query.groupID as string;
+      if (!groupID) {
+        response.send({
+          success: false,
+          message: "Missing parameters",
+          detailedMessage: {groupID},
+        });
+        return;
+      }
+      const group = await groupManager.getGroup(groupID);
+      const balances = group.getBalances();
+      // eslint-disable-next-line guard-for-in
+      for (const entries in Object.entries(balances)) {
+        const userID = entries[0];
+        const balance = Number(entries[1]);
+        if (balance > 0) {
+          const user = await loadUser(userID);
+          if (!user) {
+            continue;
+          }
+          const email = user.email;
+          await sendMail({
+            account: emailAccount.value(),
+            password: emailPassword.value(),
+            from: "Fairshare@project.com",
+            text: `Reminder open balance of ${balance} EUR`,
+            to: email,
+            subject: "Reminder of open balance",
+            html: `<b>Hi ${user.name}, 
+              you have open payments to take action on 
+              in your group "${group.data.name}". 
+              Open: ${balance} ${group.data.currency} EUR</b>`,
+          });
+        }
+      }
+  
+      response.send({success: true});
+    }
+  );
