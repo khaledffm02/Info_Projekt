c92758ae991aeb272089f36d0e36cb842a8a521d
Roldon Brown <brownroldon@gmail.com>
2025-02-15 12:54:32 +0100

    _deleteTransaction.ts bdn

---
+++ index.ts Code Debugging Breakdown/_deleteTransaction.ts
@@ -0,0 +1,22 @@
+import { onRequest } from "firebase-functions/v2/https";
+import { getUserID } from "./auth";
+import { groupManager } from "./config";
+
+export const deleteTransaction = onRequest(
+    {cors: true},
+    async (request, response) => {
+      const groupID = request.query.groupID as string;
+      const transactionID = request.query.transactionID as string;
+      if (!groupID || !transactionID) {
+        response.send({
+          success: false,
+          message: "Missing parameters",
+          detailedMessage: {groupID, transactionID},
+        });
+        return;
+      }
+      await groupManager.deleteTransaction(groupID, transactionID);
+      response.send({success: true});
+      return;
+    }
+  );
