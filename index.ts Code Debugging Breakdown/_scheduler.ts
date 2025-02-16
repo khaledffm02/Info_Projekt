e54e78803099702a561995d7d91dffa55d51f9a4
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:06:33 +0100

    index_scheduler

--- index.ts Code Debugging Breakdown/_scheduler.ts
+++ index.ts Code Debugging Breakdown/_scheduler.ts
@@ -2,12 +2,13 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Exports a scheduled function to send reminders to groups
 exports.scheduledFunctionCrontab = onSchedule("*/5 * * * *", async () => {
-    const groups = await groupManager.getGroupRemindersForDate();
+    const groups = await groupManager.getGroupRemindersForDate(); // Get groups with reminders for the current date
     const getEndpoint = (groupID: string) =>
       `https://sendreminders-icvq5uaeva-uc.a.run.app?groupID=${encodeURIComponent(groupID)}`;
     await Promise.allSettled(
-      groups.map((groupID) => fetch(getEndpoint(groupID)))
+      groups.map((groupID) => fetch(getEndpoint(groupID))) // Send reminders to all groups concurrently
     );
   });
   
