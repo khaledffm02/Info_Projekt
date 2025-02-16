d8d0aa480f160c3e3c067ff7db4bcbc128a03e70
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:18:02 +0100

    group_manager_get_group

--- groupManager.ts Code Debugging Breakdown/g_getGroup.ts
+++ groupManager.ts Code Debugging Breakdown/g_getGroup.ts
@@ -1,22 +1,16 @@
 import { Firestore } from "firebase-admin/firestore";
 import { Group, GroupJSON } from "../models/Group";
 
+// Method to get a group by its ID
+
 export async function getGroup(db: Firestore, id: string): Promise<Group> {
+  // Retrieeve the group document from the database
   const groupDoc = await db.collection("groups").doc(id).get();
   if (!groupDoc.exists) {
+    // Throw an error if the group does not exist
     throw new Error(`Group with ID ${id} not found`);
   }
+  // Return the Grouo object
   return new Group(groupDoc.id, groupDoc.data() as GroupJSON);
 }
 
-export async function getGroupByCode(db: Firestore, groupCode: string): Promise<Group | undefined> {
-  const group = await db
-    .collection("groups")
-    .where("groupCode", "==", groupCode)
-    .get();
-  if (group.docs.length === 0) {
-    return;
-  }
-  const doc = group.docs[0];
-  return new Group(doc.id, doc.data() as GroupJSON);
-}
