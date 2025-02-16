0fd7b01b60be315525cf79ff90b45bfdab1cce65
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:19:25 +0100

    group_manager_get_groups_for_users

--- groupManager.ts Code Debugging Breakdown/g_getGroupsForUsers.ts
+++ groupManager.ts Code Debugging Breakdown/g_getGroupsForUsers.ts
@@ -1,10 +1,11 @@
 import { Firestore } from "firebase-admin/firestore";
 import { Group, GroupJSON } from "../models/Group";
 
+// Method to get all groups a user is part of
 export async function getGroupsForUser(db: Firestore, userID: string): Promise<Group[]> {
   const groupDocs = await db
     .collection("groups")
-    .where(`memberIDs.${userID}`, "==", true)
+    .where(`memberIDs.${userID}`, "==", true) // Query groups where user is a part of
     .get();
   
   return groupDocs.docs.map(
