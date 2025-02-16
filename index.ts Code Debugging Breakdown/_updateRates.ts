888d4df1ec9787f37e62fdcfc255b86e747f8a35
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:05:41 +0100

    index_update_rates

--- index.ts Code Debugging Breakdown/_updateRates.ts
+++ index.ts Code Debugging Breakdown/_updateRates.ts
@@ -2,11 +2,13 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Endpoint which updates the currency rates
 export const updateRates = onRequest(
     {cors: true, secrets: [currencyKey]},
+    // Request function to update currency rates
     async (request, response) => {
-      const {userID} = await getUserID(request);
-      if (!userID) {
+      const {userID} = await getUserID(request);// Extract userID using helper function
+      if (!userID) {//Check for missing parameters
         response.send({
           success: false,
           message: "Missing parameters",
@@ -15,8 +17,8 @@ export const updateRates = onRequest(
         return;
       }
   
-      await groupManager.updateCurrencyRates(currencyKey.value());
-      response.send({success: true});
+      await groupManager.updateCurrencyRates(currencyKey.value()); // Update currency rates
+      response.send({success: true}); // Respond with success
   
       return;
     }
