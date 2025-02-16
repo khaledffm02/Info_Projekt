1e726ebf0661d02060e98b2e5dc440984e7a0d57
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:05:21 +0100

    index_user_reg

--- index.ts Code Debugging Breakdown/_userRegistration.ts
+++ index.ts Code Debugging Breakdown/_userRegistration.ts
@@ -2,18 +2,21 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Method for user registration method
 export const userRegistration = onRequest(
     {cors: true},
     async (request, response) => {
       const {userID} = await getUserID(request);
+      // Get firstName and lastName from the request query parameters
       const firstName = request.query.firstName as string;
       const lastName = request.query.lastName as string;
+      // Perform user registration eith provided details
       await groupManager.userRegistration(userID, {
         firstName,
         lastName,
         currency: "EUR",
       });
-      response.send({success: true});
+      response.send({success: true}); // Send a success response
     }
   );
   
