a5693e2a12f9ad99463ce8473041e835ca56d26b
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:09:00 +0100

    index_message_constants

---
+++ index.ts Code Debugging Breakdown/_Message Constants.ts
@@ -0,0 +1,9 @@
+export const MESSAGES = {
+    MISSING_PARAMS: "Missing parameters",
+    GROUP_NOT_FOUND: "Group not found",
+    USER_NOT_FOUND: "User not found",
+    LOGIN_ATTEMPTS: "User login attempts",
+    BALANCE_REMINDER: "Reminder of open balance",
+  };
+  
+  
