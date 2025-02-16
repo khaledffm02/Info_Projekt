cdb37bde03bfaae9deb10dfc5dfe55e6204fda51
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:20:38 +0100

    group_manager_create_group

--- groupManager.ts Code Debugging Breakdown/g_createGroup.ts
+++ groupManager.ts Code Debugging Breakdown/g_createGroup.ts
@@ -1,14 +1,19 @@
 import { Firestore } from "firebase-admin/firestore";
 import { randomString } from "../utils/random-string";
 
+//Method to create a new group
+
 export async function createGroup(
   db: Firestore,
   creatorID: string,
   currency: string,
   name?: string
 ): Promise<string> {
+  //Get the current timestamp
   const creationTimestamp = Date.now();
+  //Generate random group code
   const groupCode = randomString(6).toUpperCase();
+  // Initialize member IDs with the creator ID
   const memberIDs = { [creatorID]: true };
   const groupJSON = {
     creatorID,
