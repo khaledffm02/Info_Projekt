335cad317c73fcfed451c1201ef9b1f1b2bf13d5
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:09:54 +0100

    index_delete_trans

--- index.ts Code Debugging Breakdown/_deleteTransaction.ts
+++ index.ts Code Debugging Breakdown/_deleteTransaction.ts
@@ -2,11 +2,13 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Method/endpoint for deleting a transaction
 export const deleteTransaction = onRequest(
     {cors: true},
     async (request, response) => {
       const groupID = request.query.groupID as string;
       const transactionID = request.query.transactionID as string;
+      //Check if any parameters are missing
       if (!groupID || !transactionID) {
         response.send({
           success: false,
@@ -15,8 +17,9 @@ export const deleteTransaction = onRequest(
         });
         return;
       }
+      // Delete the transaction for the specified group and transaction ID
       await groupManager.deleteTransaction(groupID, transactionID);
-      response.send({success: true});
+      response.send({success: true}); // Respond with success on deletion
       return;
     }
   );
