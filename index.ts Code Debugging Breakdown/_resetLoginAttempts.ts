e4302683ab86a52727cb39a37748735a7460fcbb
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:09:13 +0100

    index_reset_login_attempts

--- index.ts Code Debugging Breakdown/_resetLoginAttempts.ts
+++ index.ts Code Debugging Breakdown/_resetLoginAttempts.ts
@@ -2,15 +2,16 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Exports handler function to reset login attempts for a user
 export const resetLoginAttempts = onRequest(
-    {cors: true},
+    {cors: true}, // Enable CORS for the request
     async (request, response) => {
-      const email = request.query.email as string;
+      const email = request.query.email as string; // Get the email from the request query
       try {
-        const success = await groupManager.resetLoginAttempts(email);
+        const success = await groupManager.resetLoginAttempts(email); // Reset login attempts using the email
         response.send({success});
       } catch {
-        response.send({message: "User not found"}).status(404);
+        response.send({message: "User not found"}).status(404); // If user not found, send 404 response
         return;
       }
     }
