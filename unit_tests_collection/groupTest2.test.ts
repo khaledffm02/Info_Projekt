c1a37ec5cdcbcce16981d2573eb29b9316a8a8d5
Roldon Brown <brownroldon@gmail.com>
2025-02-16 16:48:04 +0100

    Test, correcrt balance_when no transactions/Test comments added

--- unit_tests_collection/groupTest2.test.ts
+++ unit_tests_collection/groupTest2.test.ts
@@ -1,16 +1,18 @@
 import {test,expect} from "bun:test"
 import {Group} from "../functions/src/models/Group"
 
+// Initialize a new group with two members
 test('calculates balances correctly when there are no transactions', () => {
   const group = new Group('group1', {
     creatorID: 'user1',
     creationTimestamp: Date.now(),
     name: 'Test Group',
     groupCode: 'ABC123',
-    memberIDs: { user1: true, user2: true },
+    memberIDs: { user1: true, user2: true }, // Two members
     currency: 'USD',
-    transactions: {},
+    transactions: {}, // No transactions exist
   });
 
+  // Since no transactions exist, the balances should be an empty object set
   expect(group.getBalances()).toEqual({});
 });
