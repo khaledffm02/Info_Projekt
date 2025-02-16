5c69ec3eba6ed296edea10d0f0d308e122c2d88f
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:05:31 +0100

    index_user_delete

--- index.ts Code Debugging Breakdown/_userDelete.ts
+++ index.ts Code Debugging Breakdown/_userDelete.ts
@@ -2,20 +2,26 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Method for user delete
 export const userDelete = onRequest(
     {cors: true},
     async (request, response) => {
+      // Get the userID from the request
       const {userID} = await getUserID(request);
+      // Get the groups associated with the userID
       const groups = await groupManager.getGroupsForUser(userID);
+      // Check if all groups have zero balance for the user
       const hasZeroBalance = groups.every(
         (group) => group.getBalanceForUser(userID) === 0
       );
       if (!hasZeroBalance) {
+        // Send a failure response if any group has a non-zero balance
         response.send({success: false});
         return;
       }
+      // Delete the user
       await groupManager.deleteUser(userID);
-      response.send({success: userID});
+      response.send({success: userID}); //Send a success response
     }
   );
   
