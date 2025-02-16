272e9d57f68d9404ad6364ab3832f5ab1c8d69dd
Roldon Brown <brownroldon@gmail.com>
2025-02-16 14:28:14 +0100

    Added Imports (Resolve error)

--- unit_tests_collection/groupTest6.test.ts
+++ unit_tests_collection/groupTest6.test.ts
@@ -1,3 +1,5 @@
+import {test,expect} from "bun:test"
+import {Group} from "../functions/src/models/Group"
 
 test('handles undefined group name correctly', () => {
   const group = new Group('group1', {
