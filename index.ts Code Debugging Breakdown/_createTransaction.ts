b10ce7478796d2cac71ec055fe2de99689091337
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:09:34 +0100

    index_create_trans

--- index.ts Code Debugging Breakdown/_createTransaction.ts
+++ index.ts Code Debugging Breakdown/_createTransaction.ts
@@ -2,15 +2,21 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Method for creating new transaction function
 export const createTransaction = onRequest(
     {cors: true},
     async (request, response) => {
+      // Get the request parameters from the query and decode them
       const parameters = decodeURIComponent(request.query.request as string);
+      // Get the userID from the request
       const {userID} = await getUserID(request);
       if (!parameters || !userID) {
+        // Send a failure response if parameters or userID is not provided
         response.send({success: false});
         return;
       }
+
+      // Parse the request parameters
       const {groupID, title, category, user, friends, storageURL} = JSON.parse(
         parameters
       ) as {
@@ -21,7 +27,9 @@ export const createTransaction = onRequest(
         user: { id: string; value: number };
         friends: { id: string; value: number }[];
       };
+      // Validate the requires parameters
       if (!groupID || !title || !user || !friends) {
+        // Send a failure response if required parametrs are missing
         response.send({
           success: false,
           message: "Missing parameters",
@@ -29,9 +37,12 @@ export const createTransaction = onRequest(
         });
         return;
       }
+      // Calculate the total paid and spent values
       const payed = user.value;
       const spend = friends.reduce((acc, f) => acc + f.value, 0);
+      // Validate that the paid amount matches the total spent amount
       if (payed !== spend) {
+        // Send a failure response if there is a mismatch
         response.send({
           success: false,
           message: "Sum of friends' values must be equal to user's value",
@@ -39,12 +50,15 @@ export const createTransaction = onRequest(
         });
         return;
       }
+
+      // Create the transaction in the group
       const id = await groupManager.createTransaction(
         groupID,
         {title, category, storageURL},
         user,
         friends
       );
+      // Set a reminder for the group
       await groupManager.setGroupReminder(groupID);
       response.send({success: true, transactionID: id});
       return;
