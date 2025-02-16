03442e801555c9aaf319a01acf167e0072f2c1f8
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:19:01 +0100

    group_manager_delete_user

--- groupManager.ts Code Debugging Breakdown/g_deleteUser.ts
+++ groupManager.ts Code Debugging Breakdown/g_deleteUser.ts
@@ -1,5 +1,7 @@
+// Method to delete a user
+
 import { Firestore } from "firebase-admin/firestore";
 
 export async function deleteUser(db: Firestore, userID: string) {
-  await db.collection("users").doc(userID).delete();
+  await db.collection("users").doc(userID).delete(); // Delete the user document
 }
