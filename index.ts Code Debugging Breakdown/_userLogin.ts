b94e58b826e08a621010f4b728df8163e2f47838
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:05:52 +0100

    index_user_login

--- index.ts Code Debugging Breakdown/_userLogin.ts
+++ index.ts Code Debugging Breakdown/_userLogin.ts
@@ -2,11 +2,15 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Method allowing user login function
 export const userLogin = onRequest(
     {cors: true},
     async (request, response) => {
+      // Get the userID front the request
       const {userID} = await getUserID(request);
+      // Perform user login with preferred currency set to EUR
       await groupManager.userLogin(userID, {preferredCurrency: "EUR"});
+      // Send a success response
       response.send({success: true});
     }
   );
