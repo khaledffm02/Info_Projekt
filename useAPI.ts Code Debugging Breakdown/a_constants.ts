d0565abedcc1a54b4acbc0a46d124c840f74aac2
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:03:20 +0100

    use_api directory of constants

---
+++ useAPI.ts Code Debugging Breakdown/a_constants.ts
@@ -0,0 +1,18 @@
+export const BASE_API_URL = "https://icvq5uaeva-uc.a.run.app";
+
+export const ENDPOINTS = {
+  CREATE_GROUP: "groupcreate",
+  DELETE_GROUP: "groupdelete",
+  JOIN_GROUP: "groupjoin",
+  LEAVE_GROUP: "groupleave",
+  USER_LOGIN: "userlogin",
+  USER_REGISTER: "userregistration",
+  SEND_PASSWORD: "sendnewpassword",
+  CREATE_TRANSACTION: "createtransaction",
+  CONFIRM_TRANSACTION: "confirmtransaction",
+  DELETE_TRANSACTION: "deletetransaction",
+  ADD_PAYMENT: "addpayment",
+  EXTRACT_INFO: "extractinformation",
+  UPDATE_RATES: "updaterates",
+  GET_BALANCES: "getgroupbalance",
+};
