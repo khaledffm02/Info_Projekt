7492d96ce3741b5c658ef37b0d7b705707885f86
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:22:29 +0100

    group_manager_reminders

--- groupManager.ts Code Debugging Breakdown/g_reminders.ts
+++ groupManager.ts Code Debugging Breakdown/g_reminders.ts
@@ -1,16 +1,23 @@
 import { Firestore } from "firebase-admin/firestore";
 
+// Methods to set group reminders 
+
+// Calculates the timestamp for three days in the future
 export async function setGroupReminder(db: Firestore, groupID: string) {
   const futureThreeDays = 3 * 24 * 60 * 60 * 1000;
+  // Reference the reminders document in Firestore
+  // Set the reminder
   await db.collection("reminders").doc(groupID).set({
     reminder: Date.now() + futureThreeDays,
   });
 }
 
+
 export async function getGroupRemindersForDate(db: Firestore, timestamp = Date.now()): Promise<string[]> {
-  const remindersRef = db.collection("reminders");
+  const remindersRef = db.collection("reminders"); // Get all reminders that are due via the timestamp
   const reminders = await remindersRef.where("reminder", "<=", timestamp).get();
   
+  // Map reminders to group IDs and delete those reminders
   const groupsToRemind = reminders.docs.map((doc) => doc.id);
   await Promise.all(groupsToRemind.map((groupID) => remindersRef.doc(groupID).delete()));
   
