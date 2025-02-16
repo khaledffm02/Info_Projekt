cb072c1b4e7e1842c8ebe5f27a73f2d79a8a09eb
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:06:11 +0100

    index_group_change

--- index.ts Code Debugging Breakdown/_groupChange.ts
+++ index.ts Code Debugging Breakdown/_groupChange.ts
@@ -2,27 +2,28 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Cloud function to change group details
 export const groupChange = onRequest(
     {cors: true},
     async (request, response) => {
-      await getUserID(request);
-      const groupID = request.query.groupID as string;
-      if (!groupID) {
+      await getUserID(request); // Get the user ID from the request
+      const groupID = request.query.groupID as string; // Get the group ID from query parameters
+      if (!groupID) { // If group ID is missing
         response.send({success: false, message: "Missing groupID"});
         return;
       }
-      const groupName = request.query.groupName as string | undefined;
-      const groupCurrency = request.query.groupCurrency as string | undefined;
-      const group = await groupManager.getGroup(groupID);
-      if (!group) {
+      const groupName = request.query.groupName as string | undefined; // Get the optional group name
+      const groupCurrency = request.query.groupCurrency as string | undefined; // Get the optional group currency
+      const group = await groupManager.getGroup(groupID); // Get the group corresponding to the group ID
+      if (!group) { // If the group does not exist
         response.send({success: false, message: "Group not found"});
         return;
       }
-      await groupManager.changeGroup(groupID, {
+      await groupManager.changeGroup(groupID, { // Change the group details
         name: groupName,
         currency: groupCurrency,
       });
-      response.send({success: true});
+      response.send({success: true}); // Send a success response
     }
   );
   
