d2f1e9871f588e0c58c449b84634eaaf10051f6f
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:11:07 +0100

    index_send_new_passwords

--- index.ts Code Debugging Breakdown/_sendNewPassword.ts
+++ index.ts Code Debugging Breakdown/_sendNewPassword.ts
@@ -2,11 +2,16 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Method for sending new password
+
 export const sendNewPassword = onRequest(
     {cors: true, secrets: [emailAccount, emailPassword]},
     async (request, response) => {
+      // Get the email from the request query parameters
       const email = request.query.email as string;
+      // Generate a new random password
       const newPassword = "C7-" + randomString(8) + "L";
+      // Send the new password via email
       await sendMail({
         account: emailAccount.value(),
         password: emailPassword.value(),
@@ -17,7 +22,9 @@ export const sendNewPassword = onRequest(
         html: `<b>Your new password is ${newPassword}</b>`,
       });
   
+      // Change the user's password to the new password
       await changeUserPassword(email, newPassword);
+      // Send success response
       response.send({success: true});
     }
   );
