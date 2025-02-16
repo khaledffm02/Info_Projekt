741bcafae716e957b1775218dcda9c3fc6b0fc8e
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:05:04 +0100

    index_confirm_trans

--- index.ts Code Debugging Breakdown/_confirmTransaction.ts
+++ index.ts Code Debugging Breakdown/_confirmTransaction.ts
@@ -2,12 +2,14 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Endpoint to confirm a transaction within a group
 export const confirmTransaction = onRequest(
-    {cors: true},
+    {cors: true}, // Enable CORS to the endpoint
     async (request, response) => {
       const groupID = request.query.groupID as string;
       const transactionID = request.query.transactionID as string;
-      const {userID} = await getUserID(request);
+      const {userID} = await getUserID(request); // Fetch user ID from the request
+      // Check if parameters are missing
       if (!groupID || !transactionID || !userID) {
         response.send({
           success: false,
@@ -16,8 +18,9 @@ export const confirmTransaction = onRequest(
         });
         return;
       }
+      // Confirm the transaction for the specified group and transaction ID
       await groupManager.confirmTransaction(groupID, transactionID, userID);
-      response.send({success: true});
+      response.send({success: true}); // Send success response
       return;
     }
   );
