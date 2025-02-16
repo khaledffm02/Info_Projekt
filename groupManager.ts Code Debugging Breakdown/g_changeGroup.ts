38c453d74a3e84fac5877ace879823e1fb721ee5
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:18:18 +0100

    group_manager_change_group

--- groupManager.ts Code Debugging Breakdown/g_changeGroup.ts
+++ groupManager.ts Code Debugging Breakdown/g_changeGroup.ts
@@ -1,11 +1,13 @@
 import { Firestore } from "firebase-admin/firestore";
 
+// Method to update group details
+
 export async function changeGroup(
   db: Firestore,
   groupID: string,
   changes: { name?: string; currency?: string }
 ): Promise<void> {
-  if (!changes.name && !changes.currency) return;
+  if (!changes.name && !changes.currency) return; //Exit if there are no changes to make
   
   const groupRef = db.collection("groups").doc(groupID);
   await groupRef.update({
