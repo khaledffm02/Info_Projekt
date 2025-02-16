ba2082aab182f9495de57f03a09c37c740d42b39
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:09:23 +0100

    index_get_login_attempts

--- index.ts Code Debugging Breakdown/_getLoginAttempts.ts
+++ index.ts Code Debugging Breakdown/_getLoginAttempts.ts
@@ -2,15 +2,16 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Exports handler function to get attempts for a user
 export const getLoginAttempts = onRequest(
-    {cors: true},
+    {cors: true}, // Enable CORS for request
     async (request, response) => {
-      const email = request.query.email as string;
+      const email = request.query.email as string; // Get the email from the request query
       try {
-        const value = await groupManager.getLoginAttempts(email);
-        response.send({loginAttempts: value});
+        const value = await groupManager.getLoginAttempts(email); // Get login attempts using the email
+        response.send({loginAttempts: value}); // Send the login attempts count in the response
       } catch {
-        response.send({message: "User not found"}).status(404);
+        response.send({message: "User not found"}).status(404); // If user not found, send 404 response
         return;
       }
     }
