1cb401cac5f3b1928ae3b3d0770faf626938cf56
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:11:20 +0100

    index_increase_login_attempts

--- index.ts Code Debugging Breakdown/_increaseLoginAttempts.ts
+++ index.ts Code Debugging Breakdown/_increaseLoginAttempts.ts
@@ -2,15 +2,16 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Exports handler function to increase login attempts for a user
 export const increaseLoginAttempts = onRequest(
-    {cors: true},
+    {cors: true}, // Enable CORS
     async (request, response) => {
-      const email = request.query.email as string;
+      const email = request.query.email as string; // Get the email from the request query
       try {
-        const success = await groupManager.increaseLoginAttempts(email);
-        response.send({success});
+        const success = await groupManager.increaseLoginAttempts(email); // Increase login attempts using the email
+        response.send({success}); // Send success status in the response
       } catch {
-        response.send({message: "User not found"}).status(404);
+        response.send({message: "User not found"}).status(404); // If user not found, send 404 response
         return;
       }
     }
