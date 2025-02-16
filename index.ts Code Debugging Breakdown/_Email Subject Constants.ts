70d69d3446be6b937cf65a668682875010170fdb
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:10:31 +0100

    index_email_subject_constants

---
+++ index.ts Code Debugging Breakdown/_Email Subject Constants.ts
@@ -0,0 +1,8 @@
+export const EMAIL_SUBJECTS = {
+    PASSWORD_RESET: "Your Fairshare app password was reset",
+    PAYMENT_RECEIVED: "Fairshare: Payment Received",
+    INVITATION: "Invitation to join a group",
+    BALANCE_REMINDER: "Reminder of open balance",
+  };
+  
+  
