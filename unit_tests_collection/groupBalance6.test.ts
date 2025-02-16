e293b6c1adaeb6235e94d79007e11f80c20532a9
Roldon Brown <brownroldon@gmail.com>
2025-02-16 14:28:59 +0100

    Added Imports (Resolve error)

--- unit_tests_collection/groupBalance6.test.ts
+++ unit_tests_collection/groupBalance6.test.ts
@@ -1,3 +1,6 @@
+import {test,expect} from "bun:test"
+import {Transaction} from "../functions/src/models/Transaction"
+
 
 test('calculates balances correctly when all friends are unconfirmed', () => {
   const transaction = new Transaction(
