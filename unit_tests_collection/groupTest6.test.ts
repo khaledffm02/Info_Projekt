de0147678dc1579f8811dd49dfdfe028ef8e2f84
Roldon Brown <brownroldon@gmail.com>
2025-02-16 16:49:43 +0100

    Test, undefined group name/Test comments added

--- unit_tests_collection/groupTest6.test.ts
+++ unit_tests_collection/groupTest6.test.ts
@@ -1,16 +1,19 @@
 import {test,expect} from "bun:test"
 import {Group} from "../functions/src/models/Group"
 
+// Test to handle undefined group names correctly
+
+// Create a group without specifying a name
 test('handles undefined group name correctly', () => {
   const group = new Group('group1', {
     creatorID: 'user1',
     creationTimestamp: Date.now(),
-    name: undefined,
+    name: undefined, // Group name left undefined
     groupCode: 'XYZ456',
     memberIDs: { user1: true },
     currency: 'EUR',
     transactions: {},
   });
 
-  expect(group.data.name).toBeUndefined();
+  expect(group.data.name).toBeUndefined(); // Check to make sure group name remains undefined
 });
