f303af0a9c4521ee85f0c8c1ca76b19746702926
Roldon Brown <brownroldon@gmail.com>
2025-02-16 13:07:24 +0100

    index_group_delete

--- index.ts Code Debugging Breakdown/_groupDelete.ts
+++ index.ts Code Debugging Breakdown/_groupDelete.ts
@@ -2,12 +2,13 @@ import { onRequest } from "firebase-functions/v2/https";
 import { getUserID } from "./auth";
 import { groupManager } from "./config";
 
+// Function to delete a group
 export const groupDelete = onRequest(
-    {cors: true},
+    {cors: true}, // enable CORS
     async (request, response) => {
-      const groupID = request.query.groupID as string;
-      await groupManager.deleteGroup(groupID);
-      response.send({success: true});
+      const groupID = request.query.groupID as string; // Get the group ID from query parameters
+      await groupManager.deleteGroup(groupID); // Delete the group
+      response.send({success: true}); // Send a success response
     }
   );
   
